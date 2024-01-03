5b1c77bb6130fe7a272913c780a420a2deca8f9c
Oleksiy Nizitskiy <aloshanizi23@gmail.com>
2024-01-03 05:34:14 +0200

    commit

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 let historyLink = document.getElementById("to_history"),
   rulesLink = document.getElementById("to_rules"),
   materialsLink = document.getElementById("to_materials"),
+  authorLink = document.getElementById("to_author"),
   headerLink = document.getElementById("to_header");
 
 let historyBlock = document.getElementById("history"),
   rulesBlock = document.getElementById("rules"),
   materialsBlock = document.getElementById("materials"),
+  authorBlock = document.getElementById("author"),
   headerBlock = document.getElementById("header");
 
 historyLink.addEventListener("click", function (e) {
@@ -19,6 +21,14 @@ historyLink.addEventListener("click", function (e) {
 rulesLink.addEventListener("click", function (e) {
   e.preventDefault();
   rulesBlock.scrollIntoView({
+    behavior: "smooth",
+    block: "center",
+  });
+});
+
+authorLink.addEventListener("click", function (e) {
+  e.preventDefault();
+  authorBlock.scrollIntoView({
     behavior: "smooth",
     block: "center",
   });
